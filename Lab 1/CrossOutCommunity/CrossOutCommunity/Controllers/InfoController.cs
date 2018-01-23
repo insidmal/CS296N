1460fdc103dc455b1044a2c550aeeb7167e90400
insidmal <contact@conquest-marketing.com>
2018-01-23 10:56:25 -0800

    lab 1

---
+++ Lab 1/CrossOutCommunity/CrossOutCommunity/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrossOutCommunity.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ImportantLocations()
+        {
+            return View();
+        }
+        public IActionResult SignificantPeople()
+        {
+            return View();
+        }
+    }
+}
