29cf8faf42477a7d0f462c886ced70dee8ca3184
insidmal <contact@conquest-marketing.com>
2018-01-23 11:52:58 -0800

    lab 2

---
+++ Lab 2/CrossOutCommunity/CrossOutCommunity/Controllers/InfoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrossOutCommunity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrossOutCommunity.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ImportantLocations()
+        {
+            return View();
+        }
+        public IActionResult SignificantPeople()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz(Quiz q)
+        {
+            q = new Quiz();
+            return View(q);
+        }
+
+        [HttpPost]
+        public IActionResult CheckQuiz(Quiz q)
+        {
+            Quiz qA = new Models.Quiz();
+            string ans = "";
+
+            for (int i = 0; i < 5; i++)
+            {
+                ans = "";
+                if (i == 0) ans = q.q1;
+                else if (i == 1) ans = q.q2;
+                else if (i == 2) ans = q.q3;
+                else if (i == 3) ans = q.q4;
+                else if (i == 4) ans = q.q5;
+                if (ans == qA.Answers[0]) q.AnsCheck[0] = "Correct!";
+                else q.AnsCheck[0] = "Incorrect";
+                
+            }
+            return View(q);
+        }
+    }
+}
---
+++ Lab 2/CrossOutCommunity/CrossOutCommunity/Models/Quiz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossOutCommunity.Models
+{
+    public class Quiz
+    {
+        public List<string> Questions
+        {
+            get
+            {
+                return new List<string> {
+            "What Faction is Psycho Pete?",
+            "Which factions cars are hybrid designs combining features of airplanes and vintage cars?",
+            "What is the default Faction?",
+            "Which faction has access to resources and technology of the Brotherhood?",
+            "Which faction provides other factions with artifacts of the old world?" };
+            }
+        }
+        public List<string> Answers { get { return new List<string> { "Lunatics", "Nomads", "Engineers", "Steppenwolfs", "Scavengers" }; } }
+         public List<string> QuestionNums { get { return new List<string> { "q1", "q2", "q3", "q4", "q5" }; } }
+
+        public List<string> AnsCheck { get; set; }
+
+        public string q1 { get; set; }
+        public string q2 { get; set; }
+        public string q3 { get; set; }
+        public string q4 { get; set; }
+        public string q5 { get; set; }
+
+    }
+}
