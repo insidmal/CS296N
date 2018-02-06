d296680270ff6a843abc0da874bcb1ef66ceb160
insidmal <contact@conquest-marketing.com>
2018-02-06 11:29:34 -0800

    Lab 4 start

---
+++ Lab 4/CrossOutCommunity/CrossOutCommunity/Repositories/TestMessageRepository.cs
@@ -0,0 +1,32 @@
+﻿using CrossOutCommunity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossOutCommunity.Repositories
+{
+    public class TestMessageRepository : IMessageRepository
+    {
+        private List<Message> messages= new List<Message>();
+
+        public TestMessageRepository()
+        {
+            messages.Add(new Message { ContactMessage = "This is a contact message.", ContactUser = new User { Name = "John", EmailAddress = "contact@conquest-marketing.com" } });
+                messages.Add(new Message { ContactMessage = "This is a contact message.", ContactUser = new User { Name = "John", EmailAddress = "contact@conquest-marketing.com" } });
+        }
+
+        public List<Message> GetAllMessages()
+        {
+            return messages.ToList<Message>();
+        }
+
+        public Message AddMessage(Message mess)
+        {
+            messages.Add(mess);
+            return mess;
+        }
+
+
+    }
+}
---
+++ Lab 4/CrossOutCommunity/CrossOutCommunity/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CrossOutCommunity.Models;
+using Microsoft.EntityFrameworkCore;
+using CrossOutCommunity.Repositories;
+
+namespace BookInfo.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private CCDbContext context;
+
+        public UserRepository(CCDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return context.Users.ToList<User>();
+        }
+
+        public User GetUserByName(string user)
+        {
+            return context.Users.First(b => b.Name == user);
+        }
+
+  
+        public User AddUser(User user)
+        {
+            context.Users.Add(user);
+            return user;
+        }
+
+        public int Update(User user)
+        {
+            context.Users.Update(user);
+            return context.SaveChanges();
+        }
+    }
+}
