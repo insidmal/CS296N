3913f738d92731aa91a3791d6189aee344b3b543
John Bell <insidmal@insidmal.com>
2018-02-08 20:44:50 -0800

    lab 3 part 1 completed

---
+++ Lab 3/WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+
+        class ModelCompleteFakeRepostory : IRepository
+        {
+
+
+            public IEnumerable<Product> Products { get; set; }
+
+            public void AddProduct(Product p)
+            {
+                //nothing
+            }
+        }
+
+
+
+
+
+
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelISComplete(Product[] products)
+        {
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepostory()
+            {
+                Products = products
+            };
+        
+        
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        class ModelCompleteFakeRepositoryPricesUnder50: IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product { Name = "P1", Price=5M },
+                new Product { Name = "P2", Price=48.95M },
+                new Product { Name = "P3", Price=19.50M },
+                new Product { Name = "P3", Price=34.95M }
+            };
+            public void AddProduct(Product p)
+            {
+                //nothing
+            }
+        }
+
+        [Fact]
+        public void IndexActionModelIsCompletePricesUnder50()
+        {
+            var controller = new HomeController(); controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            Assert.Equal(controller.Repository.Products, model, Comparer.Get<Product>((P1, P2) => P1.Name == P2.Name && P1.Price == P2.Price)); 
+        }
+
+    }
+}
---
+++ Lab 3/WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Name = "New Name";
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Price = 200M;
+            Assert.Equal(200M, p.Price);
+        }
+
+    }
+}
