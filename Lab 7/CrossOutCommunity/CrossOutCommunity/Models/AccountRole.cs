8beca78981364a75c94943ab3b147f3a0fc8103b
John Bell <insidmal@insidmal.com>
2018-03-15 00:25:08 -0700

    lab 7, added role models
    accountrole connects account to role, role is the roles list

---
+++ Lab 7/CrossOutCommunity/CrossOutCommunity/Models/AccountRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossOutCommunity.Models
+{
+    public class AccountRole
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<User> Members { get; set; }
+        public IEnumerable<User> NonMembers { get; set; }
+    }
+}
