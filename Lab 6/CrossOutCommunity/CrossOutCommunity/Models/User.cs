765e53c450136fef1e57a0872a18cc9359369f56
John Bell <insidmal@insidmal.com>
2018-03-14 23:48:43 -0700

    Lab 6!! NAILED IT!!

---
+++ Lab 6/CrossOutCommunity/CrossOutCommunity/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrossOutCommunity.Models
+{
+    public class User
+    {
+        private List<Message> messages = new List<Message>();
+        public List<Message> Messages { get { return messages; } }
+
+
+
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Lab 6/CrossOutCommunity/CrossOutCommunity/Repositories/IMessageRepository.cs
@@ -0,0 +1,15 @@
+﻿using CrossOutCommunity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossOutCommunity.Repositories
+{
+    public interface IMessageRepository
+    {
+        //IQueryable<Message> GetAllMessages();
+        List<Message> GetAllMessages();
+        Message AddMessage(Message m);
+    }
+}
