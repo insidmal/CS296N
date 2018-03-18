f24cd000eb2c1d540a819d25a4ab7d0b3ea5ec0c
John Bell <insidmal@insidmal.com>
2018-03-18 13:15:13 -0700

    updating startup and packages

---
+++ Term Project/BOTSwebsite/BOTSwebsite/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using BOTSwebsite.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BOTSwebsite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BotsDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("BotsDbContext")));
+            services.AddMvc();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IMessageRepository, MessageRepository>();
+
+
+            services.AddIdentity<Account, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                //opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+
+            }).AddEntityFrameworkStores<BotsDbContext>()
+                          .AddDefaultTokenProviders();
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();   // Must precede app.UseMvc!!!
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+        }
+    }
+}
