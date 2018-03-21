44b8182a80bd2440feda7055335aec31a05e33b5
John Bell <insidmal@insidmal.com>
2018-03-21 00:10:21 -0700

    final unit tests

---
+++ Term Project/BuyOurTShirts/BuyOurTShirts.Tests/MediaRepoTests.cs
@@ -0,0 +1,52 @@
+﻿using BuyOurTShirts.Controllers;
+using BuyOurTShirts.Models;
+using BuyOurTShirts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BuyOurTShirts.Tests
+{
+    public class MediaRepoTests
+    {
+        IMediaRepository repo;
+        MediaController controller;
+
+        public MediaRepoTests()
+        {
+            repo = new TestMediaRepo();
+            controller = new MediaController(repo);
+        }
+
+
+        [Fact]
+    public void AddMediaTest()
+    {
+        Assert.Empty(repo.GetAllMedia());
+            repo.Add(new Media { name = "High on the Mountain Preview", description = "A Sneak Preview of our Single High on the Mountain", mediaType = MediaType.Audio, resourceLink = "<iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/416390931&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"></iframe>", CollectionName = "Retold EP" });
+        Assert.Single(repo.GetAllMedia());
+
+    }
+    [Fact]
+    public void DeleteMediaTest()
+    {
+            Assert.Empty(repo.GetAllMedia());
+            repo.Add(new Media { name = "High on the Mountain Preview", description = "A Sneak Preview of our Single High on the Mountain", mediaType = MediaType.Audio, resourceLink = "<iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/416390931&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"></iframe>", CollectionName = "Retold EP" });
+            Assert.Single(repo.GetAllMedia());
+            repo.Delete(repo.GetAllMedia()[0].ID);
+            Assert.Empty(repo.GetAllMedia());
+
+        }
+        [Fact]
+    public void GetAllMediaTest()
+    {
+            Assert.Empty(repo.GetAllMedia());
+            repo.Add(new Media { name = "High on the Mountain Preview", description = "A Sneak Preview of our Single High on the Mountain", mediaType = MediaType.Audio, resourceLink = "<iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/416390931&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"></iframe>", CollectionName = "Retold EP" });
+            Assert.Single(repo.GetAllMedia());
+
+        }
+
+    }
+
+}
---
+++ Term Project/BuyOurTShirts/BuyOurTShirts/Repositories/TestMediaRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BuyOurTShirts.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using BuyOurTShirts.Controllers;
+
+namespace BuyOurTShirts.Repositories
+{
+    public class TestMediaRepo : IMediaRepository
+    {
+
+
+        public List<Media> MediaList = new List<Media>();
+
+
+
+
+        public int Add(Media media)
+        {
+            MediaList.Add(media);
+            return 1;
+        }
+
+        public int Delete(int id)
+        {
+            var media = MediaList.FirstOrDefault(a => a.ID == id);
+            MediaList.Remove(media);
+            return 1;
+        }
+
+        public int Edit(Media media)
+        {
+            return 1;
+        }
+
+        public List<Media> GetAllMedia() => MediaList.ToList();
+        public List<Media> GetAllMediaByType(MediaType mt) => MediaList.Where(a => a.mediaType == mt).ToList();
+
+        public Media GetMediaById(int id) => MediaList.FirstOrDefault(a => a.ID == id);
+        public List<MediaType> GetAllMediaTypes() => new List<MediaType> { MediaType.Audio, MediaType.Image, MediaType.Video };
+
+    }
+}
